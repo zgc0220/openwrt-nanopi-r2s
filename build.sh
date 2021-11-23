cc082c51090493f2ef55983a9782a60e55701c94
Guocheng Zheng <121431418@qq.com>
2021-11-23 22:45:13 +0800

    updated

--- build.sh
+++ build.sh
@@ -621,7 +621,9 @@ svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/l
 sed -i "s/..\/..\/luci.mk/\$(TOPDIR)\/feeds\/luci\/luci.mk/g" lean/luci-app-adguardhome/Makefile
 svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/adguardhome lean/adguardhome
 sed -i "s/..\/..\/lang\/golang\/golang-package.mk/\$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g" lean/adguardhome/Makefile
-curl -L https://static.adguard.com/adguardhome/release/AdGuardHome_linux_${CPU_MODEL}.tar.gz | tar zxf -
+Arch="arm64"
+latest_ver="$(curl https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest 2>/dev/null|grep -E 'tag_name' |grep -E 'v[0-9.]+' -o 2>/dev/null)"
+curl -L https://github.com/AdguardTeam/AdGuardHome/releases/download/${latest_ver}/AdGuardHome_linux_${Arch}.tar.gz | tar zxf -
 mv AdGuardHome/AdGuardHome base-files/files/usr/bin/AdGuardHome
 rm -rf AdGuardHome
 popd
