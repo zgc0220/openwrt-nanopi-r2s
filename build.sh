da5e6aa1feed8aa62e4e6e65c0190560dc5794ec
Guocheng Zheng <121431418@qq.com>
2022-03-14 23:12:56 +0800

    updated

--- build.sh
+++ build.sh
@@ -618,7 +618,7 @@ sed -i "s/option maxfreq ''/option maxfreq0 '1608000'/g" lean/luci-app-cpufreq/r
 # zerotier
 svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/zerotier lean/zerotier
 # luci-app-adguardhome
-svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-adguardhome lean/luci-app-adguardhome
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-adguardhome lean/luci-app-adguardhome
 sed -i "s/..\/..\/luci.mk/\$(TOPDIR)\/feeds\/luci\/luci.mk/g" lean/luci-app-adguardhome/Makefile
 sed -i "s/+PACKAGE_\$(PKG_NAME)_INCLUDE_binary:adguardhome//g" lean/luci-app-adguardhome/Makefile
 sed -i "s/PKG_CONFIG_DEPENDS:= CONFIG_PACKAGE_\$(PKG_NAME)_INCLUDE_binary//g" lean/luci-app-adguardhome/Makefile
