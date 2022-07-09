b6852e63fdf89e16cd026b2e1d8f4e7cfbaa5a7f
Guocheng Zheng <121431418@qq.com>
2022-07-09 13:44:30 +0800

    updated

--- scripts/check_openclash_connect.sh
+++ scripts/check_openclash_connect.sh
@@ -2,20 +2,29 @@
 DATE=$(date +%Y-%m-%d-%H:%M:%S)
 tries=0
 LOG=/tmp/check_openclash_connect.log
+YOUTUBE=youtube.com
 GITHUB=github.com
+OPENCLASH_DNS_PORT=$(cat /etc/config/openclash | grep "option dns_port" | cut -d: -f2 | awk '{ print $3}' | cut -d "'" -f 2)
 OPENCLASH_ENABLE=$(cat /etc/config/openclash | grep -m 1 "option enable" | cut -d: -f2 | awk '{ print $3}' | cut -d "'" -f 2)
 if [ ${OPENCLASH_ENABLE} -eq 1 ]; then
   while [ $tries -lt 5 ]; do
     echo $DATE check $GITHUB start >>$LOG
-    NSLOOKUP=$(nslookup $GITHUB 127.0.0.1 2>/dev/null | grep -v grep | grep 'Address:' | wc -l)
+    NSLOOKUP=$(nslookup -port=${OPENCLASH_DNS_PORT} $GITHUB 127.0.0.1 2>/dev/null | grep -v grep | grep 'Address:' | wc -l)
     if [ ${NSLOOKUP} -ne 0 ]; then
       echo $DATE check openclash connect: OK >>$LOG
       exit 0
     else
-      let tries++
-      echo $DATE tries: $tries, openclash restart >>$LOG
-      /etc/init.d/openclash restart
-      sleep 30s
+      echo $DATE check $YOUTUBE start >>$LOG
+      NSLOOKUP=$(nslookup -port=${OPENCLASH_DNS_PORT} $YOUTUBE 127.0.0.1 2>/dev/null | grep -v grep | grep 'Address:' | wc -l)
+      if [ ${NSLOOKUP} -ne 0 ]; then
+        echo $DATE check openclash connect: OK >>$LOG
+        exit 0
+      else
+        let tries++
+        echo $DATE tries: $tries, openclash restart >>$LOG
+        /etc/init.d/openclash restart
+        sleep 10
+      fi
     fi
   done
 else
